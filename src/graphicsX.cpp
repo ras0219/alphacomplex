3fc50c7a3f83d410d146f0960c669a2a877fe349
Robert Schumacher <robertallenschumacher@gmail.com>
2013-11-26 05:22:39 -0600

    Fixed bug in X11 graphics

--- src/graphicsX.cpp
+++ src/graphicsX.cpp
@@ -55,7 +55,7 @@ Graphics::Graphics() : pImpl(new GraphicsInternal()) {
   /* map (show) the window */
   XMapWindow(display, pImpl->window);
 }
-void Graphics::LoadText(const std::string msg, const std::string font_file, int font_size)
+void Graphics::LoadText(const std::string msg, const std::string font_file)
 {
   return;
 }
