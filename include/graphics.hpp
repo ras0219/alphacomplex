d88c25aacee18ebb6d99dd133e7de5dbd2b03f54
Robert Schumacher <ras0219@outlook.com>
2014-01-11 21:02:31 -0800

    Added documentation to Graphics.hpp

--- include/graphics.hpp
+++ include/graphics.hpp
@@ -56,28 +56,48 @@ struct Graphics : debug_policy_t {
     ORANGE,
     DEFAULT
   };
+  /// Draw a string at the given CHARACTER coordinates.
+  ///
+  /// @param x,y The character coordinates to draw at
+  /// @param str The string to be drawn
+  /// @param gc  The context describing color/bgcolor/font/etc
   void drawString(int x, int y, const std::string& str, Context gc = DEFAULT);
+
+  /// Draw a char at the given CHARACTER coordinates.
+  ///
+  /// @param x,y The character coordinates to draw at
+  /// @param str The character to be drawn
+  /// @param gc  The context describing color/bgcolor/font/etc
   void drawChar(int x, int y, char str, Context gc = DEFAULT);
 
-  void handle_events(struct Controller*);
+  /// Iterate through all pending input events
+  ///
+  /// @param ctrl The controller all user-based events should be redirected to
+  void handle_events(struct Controller* ctrl);
 
-  void LoadText(const std::string msg, const std::string font_file);
+  /// Called by the main loop to repaint the screen (do not call this)
   void repaint();
+  /// Called by the destructor or by a 'quit' action
   void destroy();
 
+  /// Fetches the current width from the graphics context
+  ///
+  /// @return Current width in pixels
   inline int getWidth() { return width; }
+
+  /// Fetches the current height from the graphics context
+  ///
+  /// @return Current height in pixels
   inline int getHeight() { return height; }
 
-  int width, height;
-
   deque<Widget*> c;
-
   bool destroyed = false;
 
 protected:
-
   Graphics() { }
   ~Graphics() { destroy(); }
+
+  int width, height;
 };
 
 Graphics* new_graphics();
